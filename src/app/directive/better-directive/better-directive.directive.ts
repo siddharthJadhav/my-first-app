21f044bd59940ec15c820332f722481614ee8088
Siddharth Jadhav <siddharth.jadhav2093@gmail.com>
2019-01-13 13:34:47 +0530

    add host listner

--- src/app/directive/better-directive/better-directive.directive.ts
+++ src/app/directive/better-directive/better-directive.directive.ts
@@ -1,14 +1,23 @@
-import { Directive, ElementRef, Renderer2, OnInit } from '@angular/core';
+import { Directive, ElementRef, Renderer2, OnInit, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appBetterDirective]',
 })
 export class BetterDirective implements OnInit {
 
+  @HostListener('mouseenter') mouseEnter(eventData: Event) {
+    console.log('event Data: ');
+    this.renderer.setStyle(this.elementRef.nativeElement, 'backgroundColor', 'blue');
+  }
+
+  @HostListener('mouseleave') mouseLeave(eventData: Event) {
+    this.renderer.setStyle(this.elementRef.nativeElement, 'backgroundColor', 'transparent');
+  }
+
   constructor(private renderer: Renderer2, private elementRef: ElementRef) {}
 
   ngOnInit() {
-    this.renderer.setStyle(this.elementRef.nativeElement, 'backgroundColor', 'blue');
+    // this.renderer.setStyle(this.elementRef.nativeElement, 'backgroundColor', 'blue');
   }
 
 }
